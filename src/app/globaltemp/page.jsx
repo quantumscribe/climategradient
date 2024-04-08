dc2f114b7d66a58141278ae563e7af18458cd45e
satish <satishsinghss1910@gmail.com>
2024-04-08 18:14:26 +0530

    Added graph and real time data in global temperature

--- src/app/globaltemp/page.jsx
+++ src/app/globaltemp/page.jsx
@@ -1,7 +1,12 @@
+import GraphTemperature from "./graphtemparature";
+import Real from "./real";
+
 const GlobalTemperaturepage = () => {
     return (
       <div>
         <h1>GlobalTemperature</h1>
+        <div><GraphTemperature/></div>
+        <div><Real/></div>
       </div>
     );
   }
