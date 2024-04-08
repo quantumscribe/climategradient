066b8ee4827b50858eda101289767b19846c130a
satish <satishsinghss1910@gmail.com>
2024-04-08 20:49:44 +0530

    Graph1 and Graph2 added in upper page

--- src/app/upperatmosphere/page.jsx
+++ src/app/upperatmosphere/page.jsx
@@ -1,7 +1,12 @@
+import Graphsta from "./graphsta";
+import Graphtro from "./graphtro";
+
 const UpperAtmospherepage = () => {
     return (
       <div>
         <h1>UpperAtmosphere</h1>
+        <div><Graphtro/></div>
+        <div><Graphsta/></div>
       </div>
     );
   }
