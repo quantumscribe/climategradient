4cff6cf50570874ee91dc00043bf7ee8217df142
satish <satishsinghss1910@gmail.com>
2024-04-08 19:02:24 +0530

    added graph and real component in sealevel page

--- src/app/sealevel/page.jsx
+++ src/app/sealevel/page.jsx
@@ -1,7 +1,11 @@
+import Graphsealevel from "./graphsea";
+import Real from "./real";
 const SeaLevelpage = () => {
     return (
       <div>
         <h1>SeaLevel</h1>
+        <div><Graphsealevel/></div>
+        <div><Real/></div>
       </div>
     );
   }
