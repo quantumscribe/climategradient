c43b41061b149adca5792a23cfde4804f5736b12
satish <satishsinghss1910@gmail.com>
2024-04-13 19:49:54 +0530

    .

--- src/app/globaltemp/graphtemparature.jsx
+++ src/app/globaltemp/graphtemparature.jsx
@@ -196,7 +196,7 @@ function GraphTemperature() {
   }, []);
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-10 lg:px-10">
+    <div>
       <div style={{ position: 'relative' }}>
         <h2 className="text-xl font-bold text-white-800 px-4 py-2 border-b border-white-200">Yearly Rise in Global Temperatures</h2>
         <svg ref={svgRef} style={{ width: '100%', height: '400px' }}></svg>
